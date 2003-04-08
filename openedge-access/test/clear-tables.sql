5fe2b0cd86d270e4b663fa18239121d413c50d9d
Eelco Hillenius <no-reply@topicus.nl>
2003-04-08 08:52:35 +0000

    refactor folders
    svn path=/onderwijs/intern/openedge/openedge-access/trunk/; revision=10

--- openedge-access/test/clear-tables.sql
+++
@@ -1,8 +0,0 @@
-# clear tables for oeaccess_user
-
-delete oeaccess_user, oeaccess_group, oeaccess_group_permission,
-  oeaccess_resource, oeaccess_user_attribs, oeaccess_user_group,
-  oeaccess_user_permission
-from oeaccess_user, oeaccess_group, oeaccess_group_permission,
-  oeaccess_resource, oeaccess_user_attribs, oeaccess_user_group,
-  oeaccess_user_permission;
