a5ed8a8cb701a06af95c0794d94141f29e0804ec
Eelco Hillenius <no-reply@topicus.nl>
2003-07-11 16:02:16 +0000

    moved configexception to utils
    svn path=/onderwijs/intern/openedge/openedge-access/trunk/; revision=109

--- openedge-access/src/java/nl/openedge/access/AccessFactory.java
+++ openedge-access/src/java/nl/openedge/access/AccessFactory.java
@@ -40,6 +40,7 @@ import java.security.Policy;
 import javax.servlet.ServletContext;
 
 import nl.openedge.access.util.*;
+import nl.openedge.util.config.ConfigException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
