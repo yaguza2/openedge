51cda7f5de70a52741af015f47846cc81da77332
Eelco Hillenius <no-reply@topicus.nl>
2004-01-04 13:25:38 +0000

    *** empty log message ***
    svn path=/onderwijs/intern/openedge/openedge-maverick-framework/trunk/; revision=329

---
+++ openedge-maverick-framework/src/java/nl/openedge/maverick/framework/LogConstants.java
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Copyright (c) 2003
+ * All rights reserved.
+ */
+package nl.openedge.maverick.framework;
+
+/**
+ * Holder for constants for logging
+ * @author Eelco Hillenius
+ */
+public interface LogConstants
+{
+	/** log name for performance logging, value: nl.openedge.maverick.framework.performance */
+	public final static String PERFORMANCE_LOG = "nl.openedge.maverick.framework.performance";
+}
