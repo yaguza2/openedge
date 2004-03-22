169291f9208532cd1fb8461de2e8e30e103f1390
Sander Hofstee <sander.hofstee@topicus.nl>
2004-03-22 11:23:14 +0000

    SAPDB tables
    svn path=/onderwijs/intern/openedge/openedge-modules-impl-mailjob/trunk/; revision=478

---
+++ openedge-modules-impl-mailjob/src/etc/tables-SAPDB.sql
@@ -0,0 +1,14 @@
+DROP TABLE email_queue
+--
+CREATE TABLE email_queue (
+  id fixed(19,0) NOT NULL,
+  send_to varchar(254) NOT NULL default '',
+  message long varchar NOT NULL,
+  created long NOT NULL default '0',
+  sender varchar(254) NOT NULL default '',
+  subject varchar(254) NOT NULL default '',
+  content_type varchar(20) NOT NULL default 'text/plain',
+  status varchar(10) NOT NULL default 'new',
+  status_detail long varchar,
+  PRIMARY KEY  (id)
+)
