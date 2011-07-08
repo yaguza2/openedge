0a4b6ceacc21f6ffdac2e2053a630d61526583a7
Martijn Dashorst <martijn.dashorst@topicus.nl>
2011-07-08 21:18:34 +0000

    Betere map gekozen, tevens warnings verwijderd
    svn path=/onderwijs/intern/openedge/trunk/; revision=152902

--- openedge-access/src/main/java/nl/openedge/access/cache/HashMapCacheImpl.java
+++ openedge-access/src/main/java/nl/openedge/access/cache/HashMapCacheImpl.java
@@ -1,26 +1,23 @@
 package nl.openedge.access.cache;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Super-naieve cache implementatie
  */
 public class HashMapCacheImpl implements Cache
 {
-    /**
-     * opslaan in synchronized map om corruptie te voorkomen bij mogelijk concurrent gebruik.
-     */
-    private Map cacheMap = Collections.synchronizedMap( new HashMap<Object,Object>());
+	private ConcurrentHashMap<Object, Object> cacheMap = new ConcurrentHashMap<Object, Object>();
 
-    public Object get(Object key) throws CacheException
-    {
-	return cacheMap.get(key);
-    }
+	@Override
+	public Object get(Object key)
+	{
+		return cacheMap.get(key);
+	}
 
-    public void put(Object key, Object value) throws CacheException
-    {
-	cacheMap.put(key, value);
-    }
+	@Override
+	public void put(Object key, Object value)
+	{
+		cacheMap.put(key, value);
+	}
 }
