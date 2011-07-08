11b76668276f9ee5e39a0f8c985df707314c3df8
Martijn Dashorst <martijn.dashorst@topicus.nl>
2011-07-08 22:53:53 +0000

    Warnings, code cleanup
    svn path=/onderwijs/intern/openedge/trunk/; revision=152919

--- openedge-access/src/main/java/nl/openedge/access/UserManagerModule.java
+++ openedge-access/src/main/java/nl/openedge/access/UserManagerModule.java
@@ -1,33 +1,3 @@
-/*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Copyright (c) 2003, Open Edge B.V.
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, 
- * this list of conditions and the following disclaimer. Redistributions 
- * in binary form must reproduce the above copyright notice, this list of 
- * conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. Neither the name of OpenEdge B.V. 
- * nor the names of its contributors may be used to endorse or promote products 
- * derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
 package nl.openedge.access;
 
 import java.security.Principal;
@@ -43,164 +13,166 @@ import java.util.Set;
  */
 public interface UserManagerModule
 {
-
-	//--------------------------- USERS ----------------------------------//
-
 	/**
 	 * creates a new Principal object
-	 * @param id
+	 * 
 	 * @param name
 	 * @param password
-	 * @param attributes like first-name etc.
+	 * @param attributes
+	 *            like first-name etc.
 	 * @return Principal
 	 */
-	public Principal createUser(String name, String password, Map attributes) 
+	public UserPrincipal createUser(String name, String password, Map<String, Object> attributes)
 			throws AccessException;
 
 	/**
 	 * returns a user with this name
+	 * 
 	 * @param name
 	 * @return Principal
 	 */
-	public Principal getUser(String name) throws AccessException;
+	public UserPrincipal getUser(String name) throws AccessException;
 
 	/**
 	 * list all current users
+	 * 
 	 * @return List
 	 */
-	public List listUsers() throws AccessException;
+	public List<UserPrincipal> listUsers() throws AccessException;
 
 	/**
 	 * set password to newPassword
+	 * 
 	 * @param user
 	 * @param newPassword
 	 * @throws AccessException
 	 */
-	public void resetPassword(Principal user, String newPassword) 
-			throws AccessException;
+	public void resetPassword(Principal user, String newPassword) throws AccessException;
 
 	/**
 	 * remove user from users
+	 * 
 	 * @param user
 	 */
 	public void deleteUser(Principal user) throws AccessException;
 
-	//	--------------------------- ROLES ----------------------------------//
-
 	/**
 	 * creates a new Principal object
-	 * @param id
+	 * 
 	 * @param name
 	 * @return Principal
 	 */
-	public Principal createRole(String name) throws AccessException;
+	public RolePrincipal createRole(String name) throws AccessException;
 
 	/**
-	 * returns a role with this id
-	 * @param id
+	 * returns a role with this name
+	 * 
+	 * @param name
 	 * @return Principal
 	 */
-	public Principal getRole(String name) throws AccessException;
+	public RolePrincipal getRole(String name) throws AccessException;
 
 	/**
 	 * list all current roles
+	 * 
 	 * @return List
 	 */
-	public List listRoles() throws AccessException;
+	public List<RolePrincipal> listRoles() throws AccessException;
 
 	/**
 	 * list users from this role
+	 * 
 	 * @param role
 	 * @return List
 	 */
-	public List listUsersInRole(Principal role) throws AccessException;
+	public List<RolePrincipal> listUsersInRole(Principal role) throws AccessException;
 
 	/**
 	 * list all roles this user belongs to
+	 * 
 	 * @param user
 	 * @return List
 	 */
-	public Set listRolesForUser(Principal user) throws AccessException;
+	public Set<RolePrincipal> listRolesForUser(Principal user) throws AccessException;
 
 	/**
 	 * adds user to role
+	 * 
 	 * @param user
 	 * @param role
 	 */
-	public void addUserToRole(Principal user, Principal role) 
-			throws AccessException;
+	public void addUserToRole(Principal user, Principal role) throws AccessException;
 
 	/**
 	 * removes user from role
+	 * 
 	 * @param user
 	 * @param role
 	 */
-	public void removeUserFromRole(Principal user, Principal role) 
-			throws AccessException;
+	public void removeUserFromRole(Principal user, Principal role) throws AccessException;
 
 	/**
 	 * remove role from roles
+	 * 
 	 * @param role
 	 */
 	public void deleteRole(Principal role) throws AccessException;
 
-	//	--------------------------- GROUPS ----------------------------------//
-
 	/**
 	 * creates a new Principal object
-	 * @param id
-	 * @param name
+	 * 
 	 * @return Principal
 	 */
-	public Principal createGroup(String name) throws AccessException;
+	public GroupPrincipal createGroup(String name) throws AccessException;
 
 	/**
-	 * returns a group with this id
-	 * @param id
-	 * @return Principal
+	 * returns a group with this name
 	 */
-	public Principal getGroup(String name) throws AccessException;
+	public GroupPrincipal getGroup(String name) throws AccessException;
 
 	/**
 	 * list all current groups
+	 * 
 	 * @return List
 	 */
-	public List listGroups() throws AccessException;
+	public List<GroupPrincipal> listGroups() throws AccessException;
 
 	/**
 	 * list users from this group
+	 * 
 	 * @param group
 	 * @return List
 	 */
-	public List listUsersInGroup(Principal group) throws AccessException;
+	public List<UserPrincipal> listUsersInGroup(Principal group) throws AccessException;
 
 	/**
 	 * list all groups this user belongs to
+	 * 
 	 * @param user
 	 * @return List
 	 */
-	public Set listGroupsForUser(Principal user) throws AccessException;
+	public Set<GroupPrincipal> listGroupsForUser(Principal user) throws AccessException;
 
 	/**
 	 * adds user to group
+	 * 
 	 * @param user
 	 * @param group
 	 */
-	public void addUserToGroup(Principal user, Principal group) 
-			throws AccessException;
+	public void addUserToGroup(Principal user, Principal group) throws AccessException;
 
 	/**
 	 * removes user from group
+	 * 
 	 * @param user
 	 * @param group
 	 */
-	public void removeUserFromGroup(Principal user, Principal group) 
-			throws AccessException;
+	public void removeUserFromGroup(Principal user, Principal group) throws AccessException;
 
 	/**
 	 * remove group from groups
+	 * 
 	 * @param group
 	 */
 	public void deleteGroup(Principal group) throws AccessException;
-
 }
--- openedge-access/src/main/java/nl/openedge/access/UserPrincipal.java
+++ openedge-access/src/main/java/nl/openedge/access/UserPrincipal.java
@@ -1,33 +1,3 @@
-/*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Copyright (c) 2003, Open Edge B.V.
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, 
- * this list of conditions and the following disclaimer. Redistributions 
- * in binary form must reproduce the above copyright notice, this list of 
- * conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. Neither the name of OpenEdge B.V. 
- * nor the names of its contributors may be used to endorse or promote products 
- * derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
 package nl.openedge.access;
 
 import java.security.Principal;
@@ -36,15 +6,14 @@ import java.util.Locale;
 import java.util.Set;
 
 /**
- * <p> This class implements the <code>Principal</code> interface
- * and represents a user.
- *
- * <p> Principals such as this <code>UserPrincipal</code>
- * may be associated with a particular <code>Subject</code>
- * to augment that <code>Subject</code> with an additional
- * identity.  Refer to the <code>Subject</code> class for more information
- * on how to achieve this.  Authorization decisions can then be based upon 
- * the Principals associated with a <code>Subject</code>.
+ * This class implements the <code>Principal</code> interface and represents a user.
+ * 
+ * <p>
+ * Principals such as this <code>UserPrincipal</code> may be associated with a particular
+ * <code>Subject</code> to augment that <code>Subject</code> with an additional identity.
+ * Refer to the <code>Subject</code> class for more information on how to achieve this.
+ * Authorization decisions can then be based upon the Principals associated with a
+ * <code>Subject</code>.
  * 
  * @author E.F. Hillenius
  * @see java.security.Principal
@@ -52,43 +21,37 @@ import java.util.Set;
  */
 public class UserPrincipal implements Principal, java.io.Serializable
 {
+	private static final long serialVersionUID = 1L;
 
-	/** name */
 	protected String name;
 
 	/** encrypted password... carefull with this */
 	protected String password;
 
 	/** roles that user is member of */
-	protected Set roles;
+	protected Set<RolePrincipal> roles;
 
 	/** roles that user is member of */
-	protected Set groups;
-	
+	protected Set<GroupPrincipal> groups;
+
 	/** the prefered locale setting */
 	protected Locale preferedLocale = null;
 
-	/**
-	 * Default constructor.
-	 */
 	public UserPrincipal()
 	{
-		// do nothing here
 	}
 
 	/**
 	 * Create a UserPrincipal with a username.
-	 *
-	 * <p>
-	 *
-	 * @param name the username for this user.
-	 *
-	 * @exception NullPointerException if the <code>name</code>
-	 *			is <code>null</code>.
+	 * 
+	 * @param name
+	 *            the username for this user.
+	 * 
+	 * @exception NullPointerException
+	 *                if the <code>name</code> is <code>null</code>.
 	 */
 	public UserPrincipal(String name)
 	{
-
 		if (name == null)
 		{
 			throw new NullPointerException("name is not allowed to be null");
@@ -96,79 +59,50 @@ public class UserPrincipal implements Principal, java.io.Serializable
 		this.name = name;
 	}
 
-	/**
-	 * Return the username for this <code>UserPrincipal</code>.
-	 *
-	 * <p>
-	 *
-	 * @return the username for this <code>UserPrincipal</code>
-	 */
+	@Override
 	public String getName()
 	{
 		return name;
 	}
 
-	/**
-	 * @param name
-	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 
-	/**
-	 * @return String
-	 */
 	public String getPassword()
 	{
 		return password;
 	}
 
-	/**
-	 * @param password
-	 */
 	public void setPassword(String password)
 	{
 		this.password = password;
 	}
 
-	/**
-	 * @return List
-	 */
-	public Set getRoles()
+	public Set<RolePrincipal> getRoles()
 	{
 		return roles;
 	}
 
-	/**
-	 * Sets the roles.
-	 * @param roles The roles to set
-	 */
-	public void setRoles(Set roles)
+	public void setRoles(Set<RolePrincipal> roles)
 	{
 		this.roles = roles;
 	}
 
-	/**
-	 * add role
-	 * @param role
-	 */
 	public void addRole(Principal role)
 	{
 		if (role instanceof RolePrincipal)
 		{
+			RolePrincipal rolePrincipal = (RolePrincipal) role;
 			if (this.roles == null)
 			{
-				this.roles = new HashSet();
+				this.roles = new HashSet<RolePrincipal>();
 			}
-			this.roles.add(role);
+			this.roles.add(rolePrincipal);
 		}
 	}
 
-	/**
-	 * remove role
-	 * @param role
-	 */
 	public void removeRole(Principal role)
 	{
 		if (this.roles != null && (role instanceof RolePrincipal))
@@ -177,10 +111,6 @@ public class UserPrincipal implements Principal, java.io.Serializable
 		}
 	}
 
-	/**
-	 * does this user has given role?
-	 * @param role
-	 */
 	public boolean containsRole(Principal role)
 	{
 		if (this.roles != null && (role instanceof RolePrincipal))
@@ -193,42 +123,29 @@ public class UserPrincipal implements Principal, java.io.Serializable
 		}
 	}
 
-	/**
-	 * @return Set
-	 */
-	public Set getGroups()
+	public Set<GroupPrincipal> getGroups()
 	{
 		return groups;
 	}
 
-	/**
-	 * @param groups
-	 */
-	public void setGroups(Set groups)
+	public void setGroups(Set<GroupPrincipal> groups)
 	{
 		this.groups = groups;
 	}
 
-	/**
-	 * add group
-	 * @param group
-	 */
 	public void addGroup(Principal group)
 	{
 		if (group instanceof GroupPrincipal)
 		{
+			GroupPrincipal groupPrincipal = (GroupPrincipal) group;
 			if (this.groups == null)
 			{
-				this.groups = new HashSet();
+				this.groups = new HashSet<GroupPrincipal>();
 			}
-			this.groups.add(group);
+			this.groups.add(groupPrincipal);
 		}
 	}
 
-	/**
-	 * remove group
-	 * @param group
-	 */
 	public void removeGroup(Principal group)
 	{
 		if (this.groups != null && (group instanceof GroupPrincipal))
@@ -237,13 +154,9 @@ public class UserPrincipal implements Principal, java.io.Serializable
 		}
 	}
 
-	/**
-	 * does this user has given role?
-	 * @param group
-	 */
 	public boolean containsGroup(GroupPrincipal group)
 	{
-		if (this.groups != null && (group instanceof GroupPrincipal))
+		if (this.groups != null && group != null)
 		{
 			return this.groups.contains(group);
 		}
@@ -253,32 +166,24 @@ public class UserPrincipal implements Principal, java.io.Serializable
 		}
 	}
 
-	/**
-	 * Return a string representation of this <code>UserPrincipal</code>.
-	 *
-	 * <p>
-	 *
-	 * @return a string representation of this <code>UserPrincipal</code>.
-	 */
+	@Override
 	public String toString()
 	{
 		return "UserPrincipal: " + name;
 	}
 
 	/**
-	 * Compares the specified Object with this <code>UserPrincipal</code>
-	 * for equality.  Returns true if the given object is also a
-	 * <code>UserPrincipal</code> and the two UserPrincipals
-	 * have the same username.
-	 *
-	 * <p>
-	 *
-	 * @param o Object to be compared for equality with this
-	 *		<code>UserPrincipal</code>.
-	 *
+	 * Compares the specified Object with this <code>UserPrincipal</code> for equality.
+	 * Returns true if the given object is also a <code>UserPrincipal</code> and the two
+	 * UserPrincipals have the same username.
+	 * 
+	 * @param o
+	 *            Object to be compared for equality with this <code>UserPrincipal</code>.
+	 * 
 	 * @return true if the specified Object is equal equal to this
-	 *		<code>UserPrincipal</code>.
+	 *         <code>UserPrincipal</code>.
 	 */
+	@Override
 	public boolean equals(Object o)
 	{
 		if (o == null)
@@ -289,41 +194,26 @@ public class UserPrincipal implements Principal, java.io.Serializable
 
 		if (!(o instanceof UserPrincipal))
 			return false;
-		UserPrincipal that = (UserPrincipal)o;
+		UserPrincipal that = (UserPrincipal) o;
 
 		if (this.getName().equals(that.getName()))
 			return true;
 		return false;
 	}
 
-	/**
-	 * Return a hash code for this <code>UserPrincipal</code>.
-	 *
-	 * <p>
-	 *
-	 * @return a hash code for this <code>UserPrincipal</code>.
-	 */
+	@Override
 	public int hashCode()
 	{
 		return name.hashCode();
 	}
 
-	/**
-	 * get the prefered locale
-	 * @return Locale the prefered locale
-	 */
 	public Locale getPreferedLocale()
 	{
 		return preferedLocale;
 	}
 
-	/**
-	 * set the prefered locale
-	 * @param locale the prefered locale
-	 */
 	public void setPreferedLocale(Locale locale)
 	{
 		preferedLocale = locale;
 	}
-
 }
