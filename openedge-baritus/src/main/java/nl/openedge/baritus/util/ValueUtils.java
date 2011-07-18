24d56fbd0118c6e21e0728da48e5fe1c757157ad
Martijn Dashorst <martijn.dashorst@topicus.nl>
2011-07-18 11:22:05 +0000

    Zarro warnings
    svn path=/onderwijs/intern/openedge/trunk/; revision=154073

--- openedge-baritus/src/main/java/nl/openedge/baritus/util/ValueUtils.java
+++ openedge-baritus/src/main/java/nl/openedge/baritus/util/ValueUtils.java
@@ -34,12 +34,12 @@ import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import nl.openedge.baritus.ConverterRegistry;
 import nl.openedge.baritus.converters.ConversionException;
 import nl.openedge.baritus.converters.Converter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Misc utility methods for handling values
@@ -48,35 +48,33 @@ import nl.openedge.baritus.converters.Converter;
  */
 public final class ValueUtils
 {
-    
-    /* logger */
-    private static Logger log = LoggerFactory.getLogger(ValueUtils.class);
+
+	/* logger */
+	private static Logger log = LoggerFactory.getLogger(ValueUtils.class);
 
 	/**
 	 * check if the value is null or empty
-	 * @param value object to check on
-	 * @return true if value is not null AND not empty (e.g. 
-	 * in case of a String or Collection)
+	 * 
+	 * @param value
+	 *            object to check on
+	 * @return true if value is not null AND not empty (e.g. in case of a String or
+	 *         Collection)
 	 */
 	public static boolean isNullOrEmpty(Object value)
 	{
 		if (value instanceof String)
 		{
-			return (value == null || (((String)value).trim().equals("")));
+			return "".equals(((String) value).trim());
 		}
 		if (value instanceof Object[])
 		{
-			if (value == null)
+			if (((Object[]) value).length == 0)
 			{
 				return true;
 			}
-			else if (((Object[])value).length == 0)
+			else if (((Object[]) value).length == 1)
 			{
-				return true;
-			}
-			else if (((Object[])value).length == 1)
-			{
-				return isNullOrEmpty(((Object[])value)[0]);
+				return isNullOrEmpty(((Object[]) value)[0]);
 			}
 			else
 			{
@@ -85,34 +83,34 @@ public final class ValueUtils
 		}
 		else if (value instanceof Collection)
 		{
-			return (value == null || (((Collection)value).isEmpty()));
+			return ((Collection< ? >) value).isEmpty();
 		}
 		else if (value instanceof Map)
 		{
-			return (value == null || (((Map)value).isEmpty()));
+			return ((Map< ? , ? >) value).isEmpty();
 		}
 		else
 		{
-			return (value == null);
+			return value == null;
 		}
 	}
 
 	/**
-	 * Convert the specified value into a String.  If the specified value
-	 * is an array, the first element (converted to a String) will be
-	 * returned.  The registered {@link Converter} for the
-	 * <code>java.lang.String</code> class will be used, which allows
-	 * applications to customize Object->String conversions (the default
+	 * Convert the specified value into a String. If the specified value is an array, the
+	 * first element (converted to a String) will be returned. The registered
+	 * {@link Converter} for the <code>java.lang.String</code> class will be used, which
+	 * allows applications to customize Object->String conversions (the default
 	 * implementation simply uses toString()).
-	 *
-	 * @param value Value to be converted (may be null)
+	 * 
+	 * @param value
+	 *            Value to be converted (may be null)
 	 */
 	public static String convertToString(Object value)
 	{
 
 		if (value == null)
 		{
-			return ((String) null);
+			return null;
 		}
 		else if (value.getClass().isArray())
 		{
@@ -123,7 +121,7 @@ public final class ValueUtils
 			value = Array.get(value, 0);
 			if (value == null)
 			{
-				return ((String) null);
+				return null;
 			}
 			else
 			{
@@ -131,8 +129,8 @@ public final class ValueUtils
 				{
 					Converter converter = ConverterRegistry.getInstance().lookup(String.class);
 					Object converted = converter.convert(String.class, value);
-					return (converted instanceof String) ? 
-						(String)converted : String.valueOf(converted);
+					return (converted instanceof String) ? (String) converted : String
+						.valueOf(converted);
 				}
 				catch (Exception e)
 				{
@@ -147,8 +145,8 @@ public final class ValueUtils
 			{
 				Converter converter = ConverterRegistry.getInstance().lookup(String.class);
 				Object converted = converter.convert(String.class, value);
-				return (converted instanceof String) ? 
-					(String)converted : String.valueOf(converted);
+				return (converted instanceof String) ? (String) converted : String
+					.valueOf(converted);
 			}
 			catch (Exception e)
 			{
